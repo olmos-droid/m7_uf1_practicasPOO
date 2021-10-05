5ad0f2810198f7be2ca8952e46691832e3d6916d
josep <olmosescolatreball@gmail.com>
2021-10-05 17:45:34 +0200

    fin de clase

---
+++ app/Class/PersonaClass.php
@@ -0,0 +1,31 @@
+    <?php
+    class Persona
+    {
+
+        private $name;
+        private $surname;
+        private $adress;
+        private $comments;
+        private $foto;
+
+	function getName(){
+        return $this->name;
+    }
+    function getSurname(){
+        return $this->surname;
+    }
+    function getAdress(){
+        return $this->adress;
+    }
+    function getComments(){
+        return $this->comments;
+    }
+    function getFoto(){
+        return $this->foto;
+    }
+    
+    
+    
+    
+    }
+	
