d13132e31bf8f56d60524b4146bcbe283f5af7f9
josep <olmosescolatreball@gmail.com>
2021-10-17 19:28:02 +0200

    ok a los alerts dentro de containers

--- app/Gallery/index.php
+++ app/Gallery/index.php
@@ -1,10 +1,20 @@
 <?php
 include_once('_header.php');
 if (isset($_GET['upload']) && $_GET['upload'] == "success") {
-    echo "<div class='alert alert-success alert-dismissible'><h2>Picture Upload</h2></div>";
+    echo "
+    <div class='container'>
+    <div class='alert alert-success alert-dismissible'>
+    <h2>Picture Upload</h2>
+    </div>
+    </div>";
 }
 if (isset($_GET['upload']) && $_GET['upload'] == "error") {
-    echo '<div class="alert alert-danger" role="alert"><h3>' . $_GET["msg"] . '</h3></div>';
+    echo "
+    <div class='container'>
+    <div class='alert alert-danger' role='alert'>
+    <h3>".$_GET['msg']."</h3>
+    </div>
+    </div>";
 }
 ?>
 <div class="container">
@@ -14,7 +24,9 @@ if (isset($_GET['upload']) && $_GET['upload'] == "error") {
                 <a type="button" class="btn btn-primary" href="addPicture.php">Add picture</a>
                 <?php
                 if (file_exists("./fotos.txt")) {
-                    echo '<a type="button" class="btn btn-success" href="gallery.php?ruta=./fotos.txt">View Gallery</a>';
+                    echo '<a type="button" class="btn btn-success " href="gallery.php?ruta=./fotos.txt">View Gallery</a>';
+                }else{
+                echo '<a type="button" class="btn btn-success" href="gallery.php?ruta=./fotos.txt" disabled >View Gallery</a>';
                 }
                 ?>
             </div>
